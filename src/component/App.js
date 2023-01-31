88cde583cd406d3f9de799cbdb8b82d06464b2a0
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-01-31 15:31:16 +0300

    TMDB rest api

---
+++ src/component/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+import axios from "axios";
+// import dotenv from "dotenv";
+
+
+
+console.log(process.env.REACT_APP_API_KEY);
+
+class App extends React.Component {
+
+    state = {
+        movies: [],
+
+        searchQuery: ""
+    }
+
+    async componentDidMount() {
+        const response = await axios.get(`https://api.themoviedb.org/3/list/8236634?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+        console.log(response.data.items);
+        this.setState({movies: response.data.items})
+    }
+
+    deleteMovie = async (movie) => {
+
+        axios.post(`https://api.themoviedb.org/3/list/8236634/remove_item?media_id=${movie.id}&session_id=${process.env.REACT_APP_SESSION_ID}&api_key=${process.env.REACT_APP_API_KEY}`)
+
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        )
+
+        this.setState(state => ({
+            movies: newMovieList
+        }))
+    }
+
+    searchMovie = (event) => {
+        this.setState({searchQuery: event.target.value})
+    }
+
+
+    render() {
+
+        let filteredMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+            }
+        )
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <SearchBar
+                            searchMovieProp={this.searchMovie}
+                        />
+                    </div>
+                </div>
+                <MovieList
+                    movies={filteredMovies}
+                    deleteMovieProp = {this.deleteMovie}
+                />
+            </div>
+        )
+    }
+
+}
+
+export default App;
